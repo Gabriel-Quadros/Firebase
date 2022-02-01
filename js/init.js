8153f6c68913921f4d10422c9429c2a4b472db4f
Gaba034 <gabiiinski@hotmail.com>
2022-01-31 21:02:07 -0300

    first commit

---
+++ js/init.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDSMnoRAm5Sgt54MF7W5IR6yNkYcR1Excg",
+  authDomain: "todolist-29258.firebaseapp.com",
+  projectId: "todolist-29258",
+  storageBucket: "todolist-29258.appspot.com",
+  messagingSenderId: "853289445490",
+  appId: "1:853289445490:web:68e5bd30f50d9620e07aeb",
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
